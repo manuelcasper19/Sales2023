f252cf4f6a43e2f484512fb02cb1d1197dcc2153
Stiven Hernandez <casper.sa690@gmail.com>
2023-02-12 16:34:15 -0500

    Bd sales created with table countries

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name= "País")]
+        [Required(ErrorMessage ="El cambo {0} es necesario.")]
+        [MaxLength(100, ErrorMessage ="El campo {0} no puede tener más de {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+
+
+    }
+}
