a545905b60e0f3d7be7256e79005c7cad74336d4
STIVEN HERNANDEZ <49258857+manuelcasper19@users.noreply.github.com>
2023-03-12 16:55:40 -0500

    Security frontend finished

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+          
+            var stivenUser = new ClaimsIdentity(new List<Claim>
+        {
+            new Claim("FirstName", "Stiven"),
+            new Claim("LastName", "Hernandez"),
+            new Claim(ClaimTypes.Name, "stiven@yopmail.com"),
+            new Claim(ClaimTypes.Role, "Admin")
+        },
+            authenticationType: "test");
+            var anonimous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(stivenUser)));
+        }
+    }
+
+}
--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,15 +1,21 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Auth;
 using Sales.WEB.Repository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7295/") });
+//Http lo cambiamos de scope a singleton para no estar enviando token de acceso
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7295/") });
 builder.Services.AddScoped<IRepository,  Repository>();
 builder.Services.AddSweetAlert2();
+//Inyecatmos las autenticaciones del usuario
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 
 await builder.Build().RunAsync();
